bcebdf030a11a7a44b1df4d2e48cb87db4aa1873
Pedro Rojo <69168165+tecnosisnet@users.noreply.github.com>
2022-11-03 06:31:04 -0300

    Update insert-db.sql

--- BASE DE DATOS/insert-db.sql
+++ BASE DE DATOS/insert-db.sql
@@ -7,8 +7,8 @@ insert into `insert-db`.`estado` values (2,'NO DISPONIBLE');
 -------------------------------------------------------
 -- Tabla OPERATORIA COMERCIAL - INSERTANDO DATOS 
 -------------------------------------------------------
-insert into `insert-db`.`operatoria comercial` values (1,'VENTA');
-insert into `insert-db`.`operatoria comercial` values (2,'ALQUILER');
+insert into `bd_inmobiliaria`.`operatoria comercial` values (1,'VENTA');
+insert into `bd_inmobiliaria`.`operatoria comercial` values (2,'ALQUILER');
 
 -------------------------------------------------------
 -- Tabla PROPIETARIO - INSERTANDO DATOS 
@@ -36,11 +36,11 @@ insert into `bd_inmobiliaria`.`tipo` values (10,'JARDIN');
 -- Tabla PROPIEDAD - INSERTANDO DATOS 
 -------------------------------------------------------
 
-insert into `insert-d`.`propiedad`  values ('1', '5', '1', '2', '1', 'marcelo t de alvear 8855', 'CORDOBA', 'CORDOBA');
-insert into `insert-d`.`propiedad`  values ('2', '4', '2', '2', '2', 'San Martin 203', 'CORDOBA', 'Cordoba');
-insert into `insert-d`.`propiedad`  values ('3', '5', '2', '1', '1', 'Mariano moreno 1067', 'CORDOBA', 'CORDOBA');
-insert into `insert-d`.`propiedad`  values ('4', '4', '1', '1', '2', 'Av 9 de Julio 1757', 'Capital', 'Buenos Aires');
-insert into `insert-d`.`propiedad`  values ('5', '5', '2', '1', '3', 'RAv. Velez Sarsifield 4555', 'Formosa', 'Formosa');
-insert into `insert-d`.`propiedad`  values ('6', '5', '1', '2', '3', 'Tucuman 990', 'Santa Fe', 'Santa Fe');
-insert into `insert-d`.`propiedad`  values ('7', '5', '2', '2', '1', 'Tanti 123', 'CORDOBA', 'CORDOBA');
-insert into `insert-d`.`propiedad`  values ('8', '2', '2', '1', '1', 'Tafi  333 ', 'Salta', 'Salta')
+insert into `bd_inmobiliaria`.`propiedad`  values ('1', '5', '1', '2', '1', 'marcelo t de alvear 8855', 'CORDOBA', 'CORDOBA');
+insert into `bd_inmobiliaria`.`propiedad`  values ('2', '4', '2', '2', '2', 'San Martin 203', 'CORDOBA', 'Cordoba');
+insert into `bd_inmobiliaria`.`propiedad`  values ('3', '5', '2', '1', '1', 'Mariano moreno 1067', 'CORDOBA', 'CORDOBA');
+insert into `bd_inmobiliaria`.`propiedad`  values ('4', '4', '1', '1', '2', 'Av 9 de Julio 1757', 'Capital', 'Buenos Aires');
+insert into `bd_inmobiliaria`.`propiedad`  values ('5', '5', '2', '1', '3', 'RAv. Velez Sarsifield 4555', 'Formosa', 'Formosa');
+insert into `bd_inmobiliaria`.`propiedad`  values ('6', '5', '1', '2', '3', 'Tucuman 990', 'Santa Fe', 'Santa Fe');
+insert into `bd_inmobiliaria`.`propiedad`  values ('7', '5', '2', '2', '1', 'Tanti 123', 'CORDOBA', 'CORDOBA');
+insert into `bd_inmobiliaria`.`propiedad`  values ('8', '2', '2', '1', '1', 'Tafi  333 ', 'Salta', 'Salta')
